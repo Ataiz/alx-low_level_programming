9097d17664ac9e831e198db557c803ad11d69437
Ataiz <ataizak@gmail.com>
2023-06-18 17:39:37 +0100

    0x01-variables_if_else_while + optional done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,31 +24,32 @@
  */
 int main(void)
 {
-	int i, j, a, b, c, d;
+	int i, j;
+	int a, b, c, d;
 
-	for (i = 0 ; i <= 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		a = i / 10;
-		b = (i % 10);
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
 
-		for (j = 0 ; j <= 99; j++)
+		for (j = 0; j < 100; j++)
 		{
-			c = j / 10;
-			d = (j % 10);
-		}
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
 
-		if (a < c || (a == c && b < d))
-		{
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(' ');
-			putchar(c + '0');
-			putchar(d + '0');
-
-			if (!(a == 9 && b == 8))
+			if (a < c || (a == c && b < d))
 			{
-				putchar(',');
+				putchar(a + '0');
+				putchar(b + '0');
 				putchar(' ');
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(' ');
+					putchar(',');
+				}
 			}
 		}
 	}
